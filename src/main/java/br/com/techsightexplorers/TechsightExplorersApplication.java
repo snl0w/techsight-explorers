8e5ea76ec91462b6343cba1a54f9b484b81b07a6
Khaled <khaled.valdivino@ads.uniceplac.edu.br>
2024-08-12 16:41:09 -0300

    Primeiro commit

---
+++ src/main/java/br/com/techsightexplorers/TechsightExplorersApplication.java
@@ -0,0 +1,13 @@
+package br.com.techsightexplorers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechsightExplorersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechsightExplorersApplication.class, args);
+	}
+
+}
