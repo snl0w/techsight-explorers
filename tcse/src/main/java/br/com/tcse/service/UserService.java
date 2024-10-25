ca9cf76c58c32a835501d069951e02667c9c3d50
Daniel <danielumberto51@gmail.com>
2024-10-25 15:15:26 -0300

    :sparkles: feat: Crud do usuário

--- tcse/src/main/java/br/com/tcse/service/UserService.java
+++ tcse/src/main/java/br/com/tcse/service/UserService.java
@@ -2,7 +2,12 @@ package br.com.tcse.service;
 
 import br.com.tcse.dto.UserDto;
 
+import java.util.List;
+
 public interface UserService {
     UserDto createUser(UserDto userDto);
-
+    UserDto getUserById(Long id);
+    List<UserDto> getAllUsers();
+    UserDto updateuser(Long id, UserDto updatedUser);
+    void deleteUser(Long id);
 }
--- tcse/src/main/java/br/com/tcse/service/impl/UserServiceImpl.java
+++ tcse/src/main/java/br/com/tcse/service/impl/UserServiceImpl.java
@@ -2,11 +2,15 @@ package br.com.tcse.service.impl;
 
 import br.com.tcse.dto.UserDto;
 import br.com.tcse.entity.User;
+import br.com.tcse.exception.ResourceNotFoundException;
 import br.com.tcse.mapper.UserMapper;
 import br.com.tcse.repository.UserRepository;
 import br.com.tcse.service.UserService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @AllArgsConstructor
@@ -21,4 +25,49 @@ public class UserServiceImpl implements UserService {
         User savedUser = userRepository.save(user);
         return UserMapper.mapToUserDto(savedUser);
     }
+
+    @Override
+    public UserDto getUserById(Long id) {
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Usuário não existe: " +id));
+
+        return UserMapper.mapToUserDto(user);
+    }
+
+    @Override
+    public List<UserDto> getAllUsers(){
+        List<User> users = userRepository.findAll();
+
+        return users.stream().map((user) -> UserMapper.mapToUserDto(user))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public UserDto updateuser(Long id, UserDto updatedUser) {
+
+        User user = userRepository.findById(id).orElseThrow(
+                () -> new ResourceNotFoundException("Não existe usuário com esse id: " +id)
+        );
+
+
+        user.setUsername(updatedUser.getUsername());
+        user.setEmail(updatedUser.getEmail());
+        user.setBiography(updatedUser.getBiography());
+
+        User updatedUserObj = userRepository.save(user);
+
+        return UserMapper.mapToUserDto(updatedUserObj);
+    }
+
+    @Override
+    public void deleteUser(Long id) {
+
+        User user = userRepository.findById(id).orElseThrow(
+                () -> new ResourceNotFoundException("Não existe usuário com esse id: " +id)
+        );
+
+        userRepository.deleteById(id);
+    }
+
+
 }
