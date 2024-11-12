1cd78dacf93328436fc61f2afb63ce7a509f3a1d
Daniel <danielumberto51@gmail.com>
2024-11-12 16:31:55 -0300

    :sparkles: feat: UserService: Métodos save e findByEmail

--- src/main/java/br/com/tcse/service/UserService.java
+++ src/main/java/br/com/tcse/service/UserService.java
@@ -1,6 +1,7 @@
 package br.com.tcse.service;
 
 import br.com.tcse.dto.UserDto;
+import br.com.tcse.entity.User;
 
 import java.util.List;
 
@@ -10,4 +11,6 @@ public interface UserService {
     List<UserDto> getAllUsers();
     UserDto updateuser(Long id, UserDto updatedUser);
     void deleteUser(Long id);
+    User findByEmail(String email);
+    User save();
 }
