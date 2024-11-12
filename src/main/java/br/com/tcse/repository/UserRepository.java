bc6a2975a0bb17a505ca69d2b61407b66e130f56
Daniel <danielumberto51@gmail.com>
2024-11-12 16:25:00 -0300

    :sparkles: feat: findByEmail

--- src/main/java/br/com/tcse/repository/UserRepository.java
+++ src/main/java/br/com/tcse/repository/UserRepository.java
@@ -2,7 +2,10 @@ package br.com.tcse.repository;
 
 import br.com.tcse.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-
+    Optional<User> findByEmail(String email);
 }
