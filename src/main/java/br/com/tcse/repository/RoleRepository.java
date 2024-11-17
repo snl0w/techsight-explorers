2915879b05aaff3d65447ae1d04d45ebc8c74496
Daniel <danielumberto51@gmail.com>
2024-11-17 10:00:57 -0300

    Repository atualizado

---
+++ src/main/java/br/com/tcse/repository/RoleRepository.java
@@ -0,0 +1,8 @@
+package br.com.tcse.repository;
+
+import br.com.tcse.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Role findByName(String name);
+}
--- src/main/java/br/com/tcse/repository/UserRepository.java
+++ src/main/java/br/com/tcse/repository/UserRepository.java
@@ -6,5 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<UserBlog, Long> {
-    public UserBlog findByEmail(String email);
+    UserBlog findByEmail(String email);
 }
