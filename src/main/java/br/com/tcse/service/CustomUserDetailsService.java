b73ead2c5ce2bb74ab52edc05199437e05e4dcc6
Daniel <danielumberto51@gmail.com>
2024-11-17 10:15:41 -0300

    Classe CustomUserDetailsService

---
+++ src/main/java/br/com/tcse/service/CustomUserDetailsService.java
@@ -0,0 +1,31 @@
+package br.com.tcse.service;
+
+import br.com.tcse.model.UserBlog;
+import br.com.tcse.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.util.stream.Collectors;
+
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        UserBlog user = userRepository.findByEmail(email);
+        if (user != null) {
+            return new org.springframework.security.core.userdetails.User(user.getEmail()
+                    , user.getPassword(),
+                    user.getRoles().stream()
+                            .map((role) -> new SimpleGrantedAuthority(role.getName()))
+                            .collect(Collectors.toList()));
+        } else {
+            throw new UsernameNotFoundException("Invalid email or password");
+        }
+    }
+}
