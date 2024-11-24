4534012c6a4161e1f2340f163d196d7ef7ba0390
Daniel <danielumberto51@gmail.com>
2024-11-24 14:32:29 -0300

    Atualização de estrutura

--- src/main/java/br/com/tcse/controller/BlogController.java
+++ src/main/java/br/com/tcse/controller/BlogController.java
@@ -1,13 +1,11 @@
 package br.com.tcse.controller;
 
-import br.com.tcse.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class HomeController {
+public class BlogController {
 
     // Método para exibir a página inicial no endpoint /home
     @GetMapping("/")
--- src/main/java/br/com/tcse/security/SecurityConfig.java
+++ src/main/java/br/com/tcse/security/SecurityConfig.java
@@ -1,8 +1,7 @@
-package br.com.tcse.config;
+package br.com.tcse.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
